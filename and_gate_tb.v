a4fa574f4beb0c45014ca3ff0c40078584f22b40
Zachary <zach.aryjacob27@gmail.com>
2025-07-25 15:51:28 -0400

    AND Gate
    This is the AND Gate code I made on Verilog, and now I actually have the gtkwave in it as well

---
+++ and_gate_tb.v
@@ -0,0 +1,41 @@
+`timescale 1ns/1ps
+
+module and_gate_tb;
+
+
+    reg A;
+    reg B;
+    wire Y;
+
+
+    and_gate uut (
+        .A(A), 
+        .B(B), 
+        .Y(Y)
+    );
+
+    initial begin
+        
+        $dumpfile("and_gate_tb.vcd");
+        $dumpvars(0, and_gate_tb);
+
+        $display("Time | A | B | Y");
+        $display("------------------");
+
+
+        A = 0; B = 0; #5;
+        $display("%t   | %b | %b | %b", $time, A, B, Y);
+
+        A = 0; B = 1; #5;
+        $display("%t   | %b | %b | %b", $time, A, B, Y);
+
+        A = 1; B = 0; #5;
+        $display("%t   | %b | %b | %b", $time, A, B, Y);
+
+        A = 1; B = 1; #5;
+        $display("%t   | %b | %b | %b", $time, A, B, Y);
+
+        $stop;
+    end
+
+endmodule
